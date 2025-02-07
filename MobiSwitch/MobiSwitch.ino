19d9ee4c8816bb50a9fae8c6cb30729f0b9d78da
ItzShuvrodip <101265572+ItzShuvrodip@users.noreply.github.com>
2025-02-08 04:19:58 +0530

    Update MobiSwitch.ino

--- MobiSwitch/MobiSwitch.ino
+++ MobiSwitch/MobiSwitch.ino
@@ -1,27 +1,36 @@
 #include <WiFi.h>
-#include <HTTPClient.h>
+#include <WebServer.h>
 
-#define FIREBASE_HOST "https://mobiswitch-89d10-default-rtdb.asia-southeast1.firebasedatabase.app"
-#define FIREBASE_AUTH "NQ47wBW9Ff5c2We3KHW29usDByI4SAvbErOi7HAv"
-#define FIREBASE_PATH "/esp32/command.json?auth=" FIREBASE_AUTH
-#define FIREBASE_PATH_SSID "/esp32/wifi/ssid.json?auth=" FIREBASE_AUTH
-#define FIREBASE_PATH_PASSWORD "/esp32/wifi/password.json?auth=" FIREBASE_AUTH
+const char *ssid = "MobiSwitch";
+const char *password = "UTKARSH";
 
-void connectToWiFi(String ssid, String password) {
-    Serial.println("Connecting to WiFi: " + ssid);
-    WiFi.begin(ssid.c_str(), password.c_str());
+WebServer server(80);
 
-    while (WiFi.status() != WL_CONNECTED) {
-        Serial.print(".");
-        digitalWrite(2, LOW);
-        delay(1000);
-    }
+bool signalState = false;
 
-    if (WiFi.status() == WL_CONNECTED) {
-        Serial.println("\nConnected to WiFi!");
+void handleOn() {
+    signalState = true;
+    digitalWrite(33, HIGH);
+    Serial.println("ON");
+    server.send(200, "text/plain", "Signal ON");
+}
+
+void handleOff() {
+    signalState = false;
+    digitalWrite(33, LOW);
+    Serial.println("OFF");
+    server.send(200, "text/plain", "Signal OFF");
+}
+
+void handleStatus() {
+    server.send(200, "text/plain", signalState ? "ON" : "OFF");
+}
+
+void checkClients() {
+    if (WiFi.softAPgetStationNum() > 0) {
         digitalWrite(2, HIGH);
     } else {
-        Serial.println("\nFailed to connect!");
+        digitalWrite(2, LOW);
     }
 }
 
@@ -29,68 +38,16 @@ void setup() {
     Serial.begin(115200);
     pinMode(2, OUTPUT);
     pinMode(33, OUTPUT);
-    
-    WiFi.begin("BTS", "02468000"); // Temporary default WiFi
-    Serial.print("Connecting to Default WiFi");
-    while (WiFi.status() != WL_CONNECTED) {
-        Serial.print(".");
-        delay(1000);
-    }
-    Serial.println("\nConnected to Default WiFi!");
-
-    HTTPClient http;
-    
-    // Fetch SSID
-    http.begin(String(FIREBASE_HOST) + FIREBASE_PATH_SSID);
-    int httpCode = http.GET();
-    String ssid = "";
-    if (httpCode == 200) {
-        ssid = http.getString();
-        ssid.replace("\"", "");
-        Serial.println("SSID from Firebase: " + ssid);
-    }
-    http.end();
-    http.begin(String(FIREBASE_HOST) + FIREBASE_PATH_PASSWORD);
-    httpCode = http.GET();
-    String password = "";
-    if (httpCode == 200) {
-        password = http.getString();
-        password.replace("\"", "");
-        Serial.println("Password from Firebase: " + password);
-    }
-    http.end();
-
-    if (!ssid.isEmpty() && !password.isEmpty()) {
-        connectToWiFi(ssid, password);
-    }
+    WiFi.softAP(ssid, password);
+    Serial.println("ESP32 AP Started");
+    server.on("/on", handleOn);
+    server.on("/off", handleOff);
+    server.on("/status", handleStatus);
+    server.begin();
+    Serial.println("Server Started");
 }
 
 void loop() {
-    if (WiFi.status() == WL_CONNECTED) {
-        HTTPClient http;
-        String url = String(FIREBASE_HOST) + FIREBASE_PATH;
-
-        http.begin(url);
-        int httpCode = http.GET();
-
-        if (httpCode == 200) {
-            String payload = http.getString();
-            Serial.println("Received: " + payload);
-            if (payload=="true") {
-                digitalWrite(33, HIGH);
-                Serial.println("LED ON");
-            } else if (payload=="false") {
-                digitalWrite(33, LOW);
-                Serial.println("LED OFF");
-            }
-        } else {
-            Serial.println("Failed to connect to Firebase, HTTP Code: " + String(httpCode));
-        }
-
-        http.end();
-    } else {
-        Serial.println("WiFi Disconnected!");
-    }
-
-    delay(1);
+    server.handleClient();
+    checkClients();
 }
