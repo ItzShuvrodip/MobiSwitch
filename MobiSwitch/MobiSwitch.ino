fa71f533443c0c55990d3b008f36d8b5c8a48565
ItzShuvrodip <101265572+ItzShuvrodip@users.noreply.github.com>
2025-02-08 14:49:37 +0530

    Update MobiSwitch.ino

--- MobiSwitch/MobiSwitch.ino
+++ MobiSwitch/MobiSwitch.ino
@@ -1,53 +1,77 @@
 #include <WiFi.h>
-#include <WebServer.h>
 
 const char *ssid = "MobiSwitch";
 const char *password = "12345678";
-
-WebServer server(80);
+WiFiServer server(80);
 
 bool signalState = false;
 
-void handleOn() {
-    signalState = true;
-    digitalWrite(33, HIGH);
-    Serial.println("ON");
-    server.send(200, "text/plain", "Signal ON");
-}
-
-void handleOff() {
-    signalState = false;
-    digitalWrite(33, LOW);
-    Serial.println("OFF");
-    server.send(200, "text/plain", "Signal OFF");
-}
-
-void handleStatus() {
-    server.send(200, "text/plain", signalState ? "ON" : "OFF");
-}
-
-void checkClients() {
-    if (WiFi.softAPgetStationNum() > 0) {
-        digitalWrite(2, HIGH);
-    } else {
-        digitalWrite(2, LOW);
-    }
-}
-
 void setup() {
-    Serial.begin(115200);
-    pinMode(2, OUTPUT);
-    pinMode(33, OUTPUT);
-    WiFi.softAP(ssid, password);
-    Serial.println("ESP32 AP Started");
-    server.on("/on", handleOn);
-    server.on("/off", handleOff);
-    server.on("/status", handleStatus);
-    server.begin();
-    Serial.println("Server Started");
+  Serial.begin(115200);
+  pinMode(2, OUTPUT);
+  pinMode(33, OUTPUT);
+  WiFi.disconnect();
+  WiFi.softAP(ssid, password);
+  server.begin(); 
+  Serial.println("ESP32 AP Started & TCP Server Running");
 }
 
 void loop() {
-    server.handleClient();
-    checkClients();
+  WiFiClient client = server.available(); 
+  if (client) {
+    Serial.println("Client connected");
+    String request = "";
+
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        request += c;
+        if (c == '\n') break; 
+      }
+    }
+
+    request.trim();
+    Serial.println("Received: " + request);
+
+    if (request.startsWith("GET ")) {
+      int start = request.indexOf(" ") + 1;
+      int end = request.indexOf(" ", start); 
+      if (end > start) {
+        request = request.substring(start, end);
+      }
+    }
+
+    Serial.println("Parsed Command: " + request);
+
+    String responseText;
+    if (request == "/on") {
+      signalState = true;
+      digitalWrite(33, HIGH);
+      Serial.println("Signal ON");
+      responseText = "Signal ON";
+    } 
+    else if (request == "/off") {
+      signalState = false;
+      digitalWrite(33, LOW);
+      Serial.println("Signal OFF");
+      responseText = "Signal OFF";
+    } 
+    else if (request == "/status") {
+      responseText = signalState ? "ON" : "OFF";
+    } 
+    else {
+      responseText = "Invalid Command";
+    }
+
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-Type: text/plain");
+    client.println("Connection: close");
+    client.println();
+    client.println(responseText);
+
+    client.stop(); 
+    Serial.println("Client disconnected");
+  }
+
+  digitalWrite(2, WiFi.softAPgetStationNum() > 0 ? HIGH : LOW);
 }
